b1648b309f23fb5b8ec139ef315a42f9468aaf60
Akash <akash@mypalla.com>
2020-07-28 21:49:54 -0700

    Add Sample Code for Challenge

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,14 +1,33 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 
 public class Robot extends TimedRobot {
 
-  
+  private TalonSRX leftIntake, rightIntake;
+  private JoystickButton intakeButton;
+  private Joystick joy;
+  private boolean lastButtonVal;
+  private double startTime;
+
   public void robotInit() {
-  
+    leftIntake = new TalonSRX(0);
+    rightIntake = new TalonSRX(1);
+
+    joy = new Joystick(1);
+    intakeButton = new JoystickButton(joy, 1);
+
+    lastButtonVal = false;
+    startTime = 0;
+
   }
 
   public void robotPeriodic() {
@@ -39,6 +58,20 @@ public class Robot extends TimedRobot {
 
   
   public void teleopPeriodic() {
+
+    if(intakeButton.get() && !lastButtonVal){       // Checks that the button has just been pressed, and starts the intake
+      startTime = Timer.getFPGATimestamp();
+      leftIntake.set(ControlMode.PercentOutput, .2);
+      rightIntake.set(ControlMode.PercentOutput, .2);
+      
+    }
+
+    if(Timer.getFPGATimestamp() - startTime > 3){       // If it has been 3 seconds since the button has been pressed, stop the Intake
+      leftIntake.set(ControlMode.PercentOutput, 0);
+      rightIntake.set(ControlMode.PercentOutput, 0);
+    }
+
+    lastButtonVal = intakeButton.get();
   }
 
 
